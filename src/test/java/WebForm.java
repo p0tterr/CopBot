4e499b5f68e327d43dce9e17ce2f13977ff6974f
Marco Morales <s6movera@uni-bonn.de>
2021-02-22 14:41:58 +0100

    -Autofill added for Supreme(Edit info in WebForm file)

--- src/test/java/WebForm.java
+++ src/test/java/WebForm.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.Test;
+
 
 
 public class WebForm extends PageObject {
